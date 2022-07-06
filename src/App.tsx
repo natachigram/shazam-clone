3b94e703faaf6cd085b24222222f8d1ec0f5343e
natachigram <natachigram@gmail.com>
2022-07-06 11:07:20 +0100

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch((e.target as HTMLInputElement).value);
+  };
+
+  const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(search);
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '777b20b1c7msh0ded2ae3b59a769p162035jsneb192719e98d',
+        'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
+      },
+    };
+
+    fetch(
+      `https://shazam.p.rapidapi.com/search?term=${search}&locale=en-US&offset=0&limit=5`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setData(response))
+      .catch((err) => console.error(err));
+    console.log(data.tracks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='app_main'>
+        <div>
+          <h1>Find Your Favorite Song</h1>
+          <form onSubmit={handleSubmit}>
+            <input type='text' value={search} onChange={handleChange} />
+            <button type='submit'>Search</button>
+          </form>
+          <div>{data}</div>
+        </div>
+      </div>
     </div>
   );
 }
